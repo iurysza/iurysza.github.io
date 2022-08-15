36900f128508a604b550be135912697e3700785f
Iury Souza <iury.souza@klarna.com>
2022-08-15 21:10:23 +0200

    update video component

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,20 +1,41 @@
 import React from 'react';
+import { css } from '@emotion/react';
 
 export interface VideoProps {
   videoSrcURL: string;
   videoTitle: string;
 }
 
+function Figcaption({ children }: { children: React.ReactNode }) {
+  return (
+    <figcaption
+      css={css`
+        text-align: center;
+        font-family: '.AppleSystemUIFont', sans-serif;
+        font-size: 1.6rem;
+        font-weight: 500;
+        padding-bottom: 5%;
+      `}
+    >
+      {children}
+    </figcaption>
+  );
+}
+
 function Video({ videoSrcURL, videoTitle }: VideoProps) {
   return (
     <div className="video">
       <iframe
+        css={css`
+          width: 100%;
+        `}
         width="640"
         height="360"
         src={videoSrcURL}
         title={videoTitle}
-        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture; fullscreen"
       />
+      <Figcaption>{videoTitle}</Figcaption>
     </div>
   );
 }
--- src/pages/speaking.tsx
+++ src/pages/speaking.tsx
@@ -50,43 +50,42 @@ function Speaking() {
           </div>
         </header>
         <main id="site-main" className={`site-main ${SiteMain} ${outer}`}>
-          <article className={`${PostFull} post page ${NoImage}`}>
-            <PostFullHeader>
-              <PostFullTitle>Speaking</PostFullTitle>
-            </PostFullHeader>
+          <div css={inner}>
+            <article className={`${PostFull} post page ${NoImage}`}>
+              <PostFullHeader>
+                <PostFullTitle>Speaking</PostFullTitle>
+              </PostFullHeader>
 
-            <PostFullContent className="post-full-content">
-              <div className="post-content">
-                <p>
-                  I like speaking at conferences and at my workplace! Covering topics ranging from
-                  Kotlin and Kotlin Multiplatform, benchmarking and more. These are some of my
-                  talks.
-                </p>
-                <h2> 2022 </h2>
-                <h3> Benchmarking and other stories - Droidcon Berlin</h3>
-                <Video
-                  videoSrcURL="//player.vimeo.com/video/734760050?autopause=0&amp;autoplay=0&amp;color=00adef&amp;portrait=0&amp;byline=0&amp;title=0"
-                  videoTitle="Benchmarking and other stories"
-                />
-                <h3> Benchmarking and other stories - Droidcon San Francisco</h3>
-                <Video
-                  videoSrcURL="//player.vimeo.com/video/723376674?autopause=0&amp;autoplay=0&amp;color=00adef&amp;portrait=0&amp;byline=0&amp;title=0"
-                  videoTitle="Benchmarking and other stories"
-                />
-                <h3> Benchmarking and other stories - AndroidMakers Paris</h3>
-                <Video
-                  videoSrcURL="https://www.youtube.com/embed/zHoJEDjLtAY"
-                  videoTitle="Kotlin Multiplatform"
-                />
-                <h2> 2021 </h2>
-                <h3> Crash Course Kotlin Multiplatform - Company event</h3>
-                <Video
-                  videoSrcURL="https://drive.google.com/file/d/12iheQ3eTBrg3vYbEGWs7zRKJEkHv7Rs9/preview"
-                  videoTitle="Kotlin Multiplatform"
-                />
-              </div>
-            </PostFullContent>
-          </article>
+              <PostFullContent className="post-full-content">
+                <div className="post-content">
+                  <p>
+                    I like speaking at conferences and at my workplace!
+                    <br />
+                    Covering topics ranging from Kotlin and Kotlin Multiplatform, benchmarking and
+                    more. These are some of my talks.
+                    <h2> 2022 </h2>
+                    <Video
+                      videoSrcURL="//player.vimeo.com/video/734760050?autopause=0&amp;autoplay=0&amp;color=00adef&amp;portrait=0&amp;byline=0&amp;title=0"
+                      videoTitle=" Benchmarking and other stories - Droidcon Berlin - July 2022"
+                    />
+                    <Video
+                      videoSrcURL="//player.vimeo.com/video/723376674?autopause=0&amp;autoplay=0&amp;color=00adef&amp;portrait=0&amp;byline=0&amp;title=0"
+                      videoTitle=" Benchmarking and other stories - Droidcon Berlin - May 2022"
+                    />
+                    <Video
+                      videoSrcURL="https://www.youtube.com/embed/zHoJEDjLtAY"
+                      videoTitle=" Benchmarking and other stories - AndroidMakers Paris - April 2022"
+                    />
+                    <h2> 2021 </h2>
+                    <Video
+                      videoSrcURL="https://drive.google.com/file/d/12iheQ3eTBrg3vYbEGWs7zRKJEkHv7Rs9/preview"
+                      videoTitle="Crash Course Kotlin Multiplatform - Company event - August 2021"
+                    />
+                  </p>
+                </div>
+              </PostFullContent>
+            </article>
+          </div>
         </main>
         <Footer />
       </Wrapper>
